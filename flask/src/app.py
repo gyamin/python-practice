47a80782d708d5fa7ff31405b09a3db0126c6055
nogami <5550621+gyamin@users.noreply.github.com>
2022-09-28 01:49:20 +0900

    # Flaskお試し実施中

--- flask/src/app.py
+++ flask/src/app.py
@@ -23,3 +23,18 @@ def postal_code():
         address.append(buff)
 
     return address
+
+@app.route("/postal-code2")
+def postal_code2():
+    conn = database.engine.connect()
+    postal_code_model = PostalCode(conn)
+    rows = postal_code_model.search_address_by_postal_code2('5270205')
+
+    address = []
+    for row in rows:
+        buff = {}
+        for field in row._fields:
+            buff[field] = row._mapping[field]
+        address.append(buff)
+
+    return address
